8a127fc8dd58daffad2b0da0d0d22f004449dadf
Antonio Díaz <20223TN047@utez.edu.mx>
2024-04-01 13:29:59 -0600

    Add AsyncStorage for mobile and update request handling

--- Simna/src/config/client/axios-client.js
+++ Simna/src/config/client/axios-client.js
@@ -1,11 +1,13 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SERVER_URL = "http://localhost:8085/api";
 const APP_JSON = "application/json";
 const AxiosClient = axios.create({
   baseURL: SERVER_URL,
 });
-
+//Web:
+/*
 const requestHandler = (req) => {
   req.headers["Accept"] = APP_JSON;
   req.headers["Content-Type"] = APP_JSON;
@@ -24,3 +26,25 @@ AxiosClient.interceptors.response.use(
 
 export default AxiosClient;
 
+*/
+
+//Movil:
+const requestHandler = async (req) => {
+  req.headers["Accept"] = APP_JSON;
+  req.headers["Content-Type"] = APP_JSON;
+  const session = JSON.parse(await AsyncStorage.getItem("user"));
+  if (session?.token) req.headers["Authorization"] = `Bearer ${session.token}`;
+  return req;
+};
+
+AxiosClient.interceptors.request.use(
+  (req) => requestHandler(req),
+  (err) => Promise.reject(err)
+);
+
+AxiosClient.interceptors.response.use(
+  (res) => Promise.resolve(res.data),
+  (err) => Promise.reject(err)
+);
+
+export default AxiosClient;
--- Simna/src/router/AppRouter.jsx
+++ Simna/src/router/AppRouter.jsx
@@ -1,3 +1,5 @@
+//Web:
+/*
 import React, { useContext } from "react";
 import Menu from "../modules/auth/login/adapters/screens/Menu";
 import {
@@ -23,6 +25,46 @@ export const AppRouter = () => {
     )
   );
   return <RouterProvider router={router} />;
+};
+
+export default AppRouter;
+*/
+//Movil:
+import * as React from "react";
+import Menu from "../modules/auth/login/adapters/screens/Menu";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AuthContext from "../config/context/auth.context";
+import Login from "../modules/auth/login/adapters/screens/Login";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const Stack = createNativeStackNavigator();
+
+export const AppRouter = () => {
+  const { user } = React.useContext(AuthContext);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={user?.signed ? "Menu" : "Login"}
+        screenOptions={{
+          headerShown: false,
+          headerStyle: {
+            backgroundColor: "#9CCDDB",
+          },
+          headerTitleStyle: {
+            color: "#072D44",
+          },
+        }}
+      >
+        {user?.signed ? (
+          <Stack.Screen name="Menu" component={Menu} />
+        ) : (
+          <Stack.Screen name="Login" component={Login} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default AppRouter;
